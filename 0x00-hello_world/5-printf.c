e5fc7a72414a19aeebe6db90fbccb64675e1e240
richcode43 <ritchychiz43@gmail.com>
2022-09-09 15:54:34 -0700

    printf continue

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main  - print the string printf function
+* 
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
+* Return 0
+*/
+int main(void)
+{
+	printf("With proper grammar,but the outcome is a piece of art, \n");
+	return (0);
+}
