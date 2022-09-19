aff2c1df2efb480ab78019252df9f5cae285279c
richcode43 <ritchychiz43@gmail.com>
2022-09-19 16:08:00 -0700

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,19 +1,11 @@
 /**
-
- * swap_int - swaps the values of two integers
-
- * using two input parameters
-
- *
-
- * @a: input parameter 1
-
- * @b: input parameter 2
-
- *
-
- * Return: Nothing
-
+*swap_int - swaps the values of the two integers
+*using input parameters
+*
+*@a: input parameter 1
+*@b: input parameter 2
+*
+*Return: Nothing
 */
 void swap_int(int *a, int *b)
 {
