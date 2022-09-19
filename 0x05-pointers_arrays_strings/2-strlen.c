fb738bc20735ce3d7375e5181f09089353419049
richcode43 <ritchychiz43@gmail.com>
2022-09-19 16:13:58 -0700

    strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen - length of string
+* @s:char
+* returns: int
+*/
+
+int _strlen(char *s)
+{
+int i;
+for(i=0; s[i] != '\0'; i++)
+{
+continue;
+}
+return (i);
+}
